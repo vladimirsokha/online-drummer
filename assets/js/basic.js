c842a62475209495f2ce40e2b564613ca0ee4928
Explodeus <volodya.sokha@gmail.com>
2020-08-20 15:27:41 +0300

    Loggers removed

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -8,22 +8,18 @@ window.addEventListener("keydown", function (event) {
     switch (event.key) {
       case "Down": // IE/Edge specific value
       case "ArrowDown":
-        console.log("ARROW DOWN");
         new Audio('assets/music/kick.wav').play();
         break;
       case "Up": // IE/Edge specific value
       case "ArrowUp":
-        console.log("ARROW UP");
         new Audio('assets/music/snare.wav').play();
         break;
       case "Left": // IE/Edge specific value
       case "ArrowLeft":
-        console.log("ARROW LEFT");
         new Audio('assets/music/clap.wav').play();
         break;
       case "Right": // IE/Edge specific value
       case "ArrowRight":
-        console.log("ARROW RIGHT");
         new Audio('assets/music/hihat.wav').play();
         break;
       case "Enter":
@@ -31,7 +27,6 @@ window.addEventListener("keydown", function (event) {
         break;
       case "Esc": // IE/Edge specific value
       case "Escape":
-        console.log("ESCAPE");
         new Audio('assets/music/crash.wav').play();
         break;
       default:
